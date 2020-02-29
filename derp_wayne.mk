4080eaa097f99b133747e084011d77dab8ec5dc3
Jullian14 <45278243+Jullian14@users.noreply.github.com>
2020-02-28 21:38:01 -0300

    Update and rename carbon_wayne.mk to derp_wayne.mk
    added wayne changes

--- derp_wayne.mk
+++ derp_wayne.mk
@@ -25,10 +25,10 @@
 $(call inherit-product, device/xiaomi/wayne/device.mk)
 
 # Inherit Carbon GSM telephony parts
-$(call inherit-product, vendor/carbon/config/gsm.mk)
+#$(call inherit-product, vendor/carbon/config/gsm.mk)
 
 # Inherit Carbon product configuration
-$(call inherit-product, vendor/carbon/config/common.mk)
+$(call inherit-product, vendor/aosip/config/common_full_phone.mk)
 
 # Define first api level
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
@@ -44,6 +44,16 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Device identifier
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_NAME := carbon_wayne
+PRODUCT_NAME := derp_wayne
 PRODUCT_DEVICE := wayne
 PRODUCT_MODEL := MI 6X
+
+IS_PHONE := true
+TARGET_GAPPS_ARCH := arm64
+TARGET_INCLUDE_WIFI_EXT := true
+TARGET_INCLUDE_STOCK_ARCORE := true
+TARGET_BOOT_ANIMATION_RES := 1080
+DERP_BUILD_ZIP_TYPE := GAPPS
+
+# Official-ify
+DERP_BUILDTYPE := Official
